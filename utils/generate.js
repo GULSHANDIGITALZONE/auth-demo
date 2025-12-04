63bc46bef291b1f4f07aa88587cbbc9bbf13ee58
GDZ <154034148+GULSHANDIGITALZONE@users.noreply.github.com>
2025-12-04 20:31:39 +0530

    Update generate.js

--- utils/generate.js
+++ utils/generate.js
@@ -1,1 +1,15 @@
+const crypto = require('crypto');
+
+function generateNumericOTP(length = 6) {
+  const digits = '0123456789';
+  let otp = '';
+  for (let i = 0; i < length; i++) otp += digits[Math.floor(Math.random() * digits.length)];
+  return otp;
+}
+
+function generateTokenHex(bytes = 32) {
+  return crypto.randomBytes(bytes).toString('hex'); // secure random token
+}
+
+module.exports = { generateNumericOTP, generateTokenHex };
 
